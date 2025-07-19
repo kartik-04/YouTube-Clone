68d2b034e7205e6619b278cf5e8ff42b3d82de44
kartik-04 <04kartik04@gmail.com>
2025-07-19 22:08:07 +0530

    Starting with UserService.java

--- src/com/youtubeclone/user/UserService.java
+++ src/com/youtubeclone/user/UserService.java
@@ -1,5 +1,9 @@
 package com.youtubeclone.user;
 
-public class UserService {
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
 
+public class UserService {
+    private final Map<UUID, User> users = new HashMap<>();
 }
