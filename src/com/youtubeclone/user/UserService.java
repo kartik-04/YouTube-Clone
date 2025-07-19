0a2a331cf11561e6af45fd76b6444a6624897349
kartik-04 <04kartik04@gmail.com>
2025-07-19 23:59:17 +0530

    first function of UserService.java implemented

--- src/com/youtubeclone/user/UserService.java
+++ src/com/youtubeclone/user/UserService.java
@@ -3,7 +3,22 @@ package com.youtubeclone.user;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.UUID;
+import java.time.LocalDateTime;
+import java.time.LocalDate;
 
 public class UserService {
-    private final Map<UUID, User> users = new HashMap<>();
+    private final Map<UUID, User> userStore = new HashMap<>();
+
+    public User registerUser(String username, String passwordHash, String email, LocalDate DOB) {
+        for(User user : userStore.values()) {
+            if(user.getEmail().equalsIgnoreCase(email)){
+                return null;
+            }
+        }
+        UUID userId  = UUID.randomUUID();
+        User newUser = new User(userId, username, passwordHash, email, DOB.atStartOfDay());
+        newUser.setAccountStatus(User.AccountStatus.PENDING_EMAIL_VERIFICATION);
+        userStore.put(userId, newUser);
+        return newUser;
+    }
 }
