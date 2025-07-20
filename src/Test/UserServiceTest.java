d44b049b69f72686d386f6b90bb4943fdec27f0a
kartik-04 <04kartik04@gmail.com>
2025-07-20 06:56:12 +0530

    setting up the JUnit 5 for testing

---
+++ src/Test/UserServiceTest.java
@@ -0,0 +1,10 @@
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class UserServiceTest {
+
+    @Test
+    public void testDummy() {
+        assertEquals(2, 1 + 1);  // Just checking setup works
+    }
+}
