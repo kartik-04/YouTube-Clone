d298274fd0a9760e8e85e389678bc135e80d26fb
kartik-04 <04kartik04@gmail.com>
2025-08-24 01:43:00 +0530

    feat : Created VideoMetadata.java file with attributes and behaviours

--- src/main/java/com/youtubeclone/Models/video/VideoMetadata.java
+++ src/main/java/com/youtubeclone/Models/video/VideoMetadata.java
@@ -1,18 +1,152 @@
 package com.youtubeclone.Models.video;
 
+
+/**
+ * Represents metadata information about a video, such as
+ * length, size, caption availability, language, and quality.
+ * This class is used inside the {@link Video} model.
+ */
+
 public class VideoMetadata {
 
-    public VideoMetadata() {}
+    /** Creating the metadata for the video
+     * for example like length,
+     * quality, language , caption
+     */
+    private final int lengthSeconds;
 
-    public VideoMetadata(VideoMetadata metadata) {
+    private final double sizeMB;
+
+    private boolean caption;
+
+    private boolean downloadable;
+
+    private Language language;
+
+    private Quality quality;
+
+    public enum Language{
+        ENGLISH,
+        HINDI,
+        FRENCH,
+        SPANISH,
+        GERMAN,
+        CHINESE,
+        JAPANESE
     }
+
+    public enum Quality{
+        P144,
+        P240,
+        P360,
+        P480,
+        P720,
+        P1080,
+        P2K,
+        P4K
+    }
+
+    /** Default constructor
+     *
+     * sets the values of the videoMetadata to it's
+     * default values
+     * */
+
+    public VideoMetadata(){
+        this.lengthSeconds = 0;
+        this.sizeMB = 0;
+        this.caption = false;
+        this.downloadable = false;
+        this.language = Language.ENGLISH;
+        this.quality = Quality.P360;
+    }
+
+    /** Parameterized Constructor */
+
+    public VideoMetadata(int lengthSeconds, double sizeMB, boolean caption, boolean downloadable, Language language, Quality quality) {
+        this.lengthSeconds = lengthSeconds;
+        this.sizeMB = sizeMB;
+        this.caption = caption;
+        this.downloadable = downloadable;
+        this.language = language;
+        this.quality = quality;
+    }
+
+    /** copy constructor */
+
+    public VideoMetadata(VideoMetadata videoMetadata) {
+        this.lengthSeconds = videoMetadata.lengthSeconds;
+        this.sizeMB = videoMetadata.sizeMB;
+        this.caption = videoMetadata.caption;
+        this.downloadable = videoMetadata.downloadable;
+        this.language = videoMetadata.language;
+        this.quality = videoMetadata.quality;
+    }
+
+    /** setting up the getter and setter for the private fields */
 
     public int getLengthSeconds() {
+        return this.lengthSeconds;
     }
 
-    public int getSizeMB() {
+    public double getSizeMB() {
+        return this.sizeMB;
     }
 
-    public String getQuality() {
+    public boolean isCaption() {
+        return this.caption;
     }
+
+    /**
+     * Sets whether captions are available.
+     *
+     * @param caption true if captions are available, false otherwise
+     */
+    public void setCaption(boolean caption) {
+        this.caption = caption;
+    }
+
+    /**
+     * gets the downloadable for the video
+     *
+     * @return downloadable true if download is possible, false otherwise
+     */
+
+    public boolean isDownloadable() {
+        return this.downloadable;
+    }
+
+    /**
+     * sets the state for downloading.
+     *
+     * @param downloadable true if download is possible, false otherwise
+     */
+    public void setDownloadable(boolean downloadable) {
+        this.downloadable = downloadable;
+    }
+
+    /**
+     * @return language enum
+     */
+    public Language getLanguage() {
+        return this.language;
+    }
+
+    public void setLanguage(Language language) {
+        this.language = language;
+    }
+
+    /**
+     * Gets the quality of the video.
+     *
+     * @return the quality enum value (e.g., {@link Quality#P720})
+     */
+    public Quality getQuality() {
+        return this.quality;
+    }
+
+    public void setQuality(Quality quality) {
+        this.quality = quality;
+    }
+
 }
