cac8d8dbc2ec9338b9407e2cab85c55201a722b4
kartik-04 <04kartik04@gmail.com>
2025-08-25 03:50:27 +0530

    feat : created interface for the videoService

--- src/main/java/com/youtubeclone/services/Interfaces/video/IVideoService.java
+++ src/main/java/com/youtubeclone/services/Interfaces/video/IVideoService.java
@@ -1,5 +1,39 @@
 package com.youtubeclone.services.Interfaces.video;
 
+import com.youtubeclone.Models.user.Creator;
+import com.youtubeclone.Models.video.Video;
+import com.youtubeclone.Models.video.VideoMetadata;
+
+import java.io.InputStream;
+import java.util.List;
+import java.util.UUID;
+
 public interface IVideoService {
+
+    /**
+     * create video method goes and initialize the video and upload it.
+     * @param video
+     * @return
+     */
+    Video createVideo(Video video);
+
+    /**
+     * Create method to upload the video and use the Video class
+     */
+    void uploadVideo(UUID videoId);
+
+    byte[] downloadVideo(Video video);
+
+    void deleteVideo(Video video);
+
+    InputStream streamVideo(UUID videoId);
+
+    Video getVideoById(UUID videoId);
+
+    List<Video> getVideoByCreator(UUID creatorId);
+
+    void updateThumbnail(UUID videoId, String newThumbnail);
+
+    VideoMetadata getVideoMetadata(UUID videoId);
 
 }
