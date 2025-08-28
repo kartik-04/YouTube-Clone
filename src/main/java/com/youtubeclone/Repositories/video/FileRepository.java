6f3be2fc5503d54f93579a8c6ca408a74f67c364
kartik-04 <04kartik04@gmail.com>
2025-08-29 05:11:12 +0530

    feat : Created the new in memory storage to store the real object apart from the metadata related to the Video

---
+++ src/main/java/com/youtubeclone/Repositories/video/FileRepository.java
@@ -0,0 +1,9 @@
+package com.youtubeclone.Repositories.video;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
+
+public class FileRepository {
+    Map<UUID, byte[]> fileRepository = new HashMap<>();
+}
