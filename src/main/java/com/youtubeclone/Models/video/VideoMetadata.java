91608e48f095e042d8f893e201fb2b594fa795fe
kartik-04 <04kartik04@gmail.com>
2025-08-28 17:52:16 +0530

    Refactor : removed the enum section from the VideoMetadata.java to make it follow SRP and OCP

--- src/main/java/com/youtubeclone/Models/video/VideoMetadata.java
+++ src/main/java/com/youtubeclone/Models/video/VideoMetadata.java
@@ -25,26 +25,6 @@ public class VideoMetadata {
 
     private Quality quality;
 
-    public enum Language{
-        ENGLISH,
-        HINDI,
-        FRENCH,
-        SPANISH,
-        GERMAN,
-        CHINESE,
-        JAPANESE
-    }
-
-    public enum Quality{
-        P144,
-        P240,
-        P360,
-        P480,
-        P720,
-        P1080,
-        P2K,
-        P4K
-    }
 
     /** Default constructor
      *
