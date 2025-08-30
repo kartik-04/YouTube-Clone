a8bfef98987a5397ec0624a43451f1f9e294dac1
kartik-04 <04kartik04@gmail.com>
2025-08-30 21:29:44 +0530

    chore : added the method to the repository to store the object and variable in memory

--- src/main/java/com/youtubeclone/Repositories/video/FileRepository.java
+++ src/main/java/com/youtubeclone/Repositories/video/FileRepository.java
@@ -1,9 +1,25 @@
 package com.youtubeclone.Repositories.video;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.UUID;
 
 public class FileRepository {
     Map<UUID, byte[]> fileRepository = new HashMap<>();
+
+    public void save(UUID id, byte[] file) {
+        fileRepository.put(id, file);
+    }
+
+    public byte[] findById(UUID id) {
+        if(!fileRepository.containsKey(id)) {
+            return null;
+        }
+        return fileRepository.get(id);
+    }
+
+    public void delete(UUID id) {
+        fileRepository.remove(id);
+    }
 }
