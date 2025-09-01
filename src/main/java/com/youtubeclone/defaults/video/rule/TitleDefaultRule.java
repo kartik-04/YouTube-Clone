d8fa616f02be6814facbf27e1656ee59f8738ced
kartik-04 <04kartik04@gmail.com>
2025-09-01 20:46:56 +0530

    Feat : Created rule to check for the default value

---
+++ src/main/java/com/youtubeclone/defaults/video/rule/TitleDefaultRule.java
@@ -0,0 +1,15 @@
+package com.youtubeclone.defaults.video.rule;
+
+import com.youtubeclone.Models.video.Video;
+import com.youtubeclone.defaults.video.VideoDefaultRule;
+
+import java.util.UUID;
+
+public class TitleDefaultRule implements VideoDefaultRule {
+    @Override
+    public void apply(Video video) {
+        if(video.getTitle() == null || video.getTitle().trim().isEmpty()){
+            video.setTitle("Untitled-"+ UUID.randomUUID().toString());
+        }
+    }
+}
