775916f00b3186f7db642514c6b08f56c659e4e4
kartik-04 <04kartik04@gmail.com>
2025-09-05 19:19:44 +0530

    Feat : Used Singleton Design pattern to implement the RateLimter feature which allows 5 request per second

--- src/main/java/com/youtubeclone/config/video/ConfigSingleton.java
+++ src/main/java/com/youtubeclone/config/video/ConfigSingleton.java
@@ -1,4 +1,31 @@
 package com.youtubeclone.config.video;
 
 public class ConfigSingleton {
+    private static volatile ConfigSingleton instance = null;
+
+    private final int maxLimit;
+    private final long windowMillis;
+
+    private ConfigSingleton() {
+        this.maxLimit = 5;
+        this.windowMillis = 1000;
+    }
+
+    public static ConfigSingleton getInstance() {
+        if(instance == null){
+            synchronized (ConfigSingleton.class) {
+                if(instance == null){
+                    instance = new ConfigSingleton();
+                }
+            }
+        }
+        return instance;
+    }
+
+    public int getMaxLimit() {
+        return maxLimit;
+    }
+    public long getWindowMillis() {
+        return windowMillis;
+    }
 }
