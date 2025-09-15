9d698af4fc764f722bef4a9c4469ff87065e494c
kartik-04 <04kartik04@gmail.com>
2025-09-15 13:34:45 +0530

    Feat : Validation class which look for the length of the comment and check with maximum limit

---
+++ src/main/java/com/youtubeclone/validators/comment/rules/LengthRule.java
@@ -0,0 +1,20 @@
+package com.youtubeclone.validators.comment.rules;
+
+import com.youtubeclone.Models.comment.Comment;
+import com.youtubeclone.exceptions.ValidationException;
+import com.youtubeclone.validators.comment.CommentValidationRule;
+
+public class LengthRule implements CommentValidationRule {
+    private static final int MAX_LENGTH = 5000;
+
+    /** It helps to use the object of comment where we check if
+     * comment is empty or not.
+     * @param comment object of Comment class
+     */
+    @Override
+    public void validate(Comment comment) {
+        if(comment.getContent() != null && comment.getContent().length() > MAX_LENGTH) {
+            throw new ValidationException("Comment exceeds maximum length of " + MAX_LENGTH + " characters");
+        }
+    }
+}
