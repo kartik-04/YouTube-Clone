09ab7a19f9b41efe1e9281319be01c643c42b996
kartik-04 <04kartik04@gmail.com>
2025-09-19 14:52:01 +0530

    feat : Entity/Model for the Subscription service

---
+++ subscription-service/src/main/java/com/youtube_clone/subscription/entities/Subscription.java
@@ -0,0 +1,30 @@
+package com.youtube_clone.subscription.entities;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.NoArgsConstructor;
+import lombok.Builder;
+
+import jakarta.persistence.*;
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Entity
+@Table(name = "subscriptions")
+@Getter
+@Setter
+@NoArgsConstructor  // Fixes the no-arg constructor issue
+@AllArgsConstructor
+@Builder
+public class Subscription {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID id;
+
+    private UUID userId;
+    private UUID creatorId;
+    private LocalDateTime subscribedAt;
+    private boolean active;
+}
