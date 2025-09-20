267b15480f957437fa6770d103bb4e03ba2e3be0
kartik-04 <04kartik04@gmail.com>
2025-09-20 20:53:31 +0530

    feat : implementation of the service class

---
+++ subscription-service/src/main/java/com/youtube_clone/subscription/services/SubscriptionServiceImpl.java
@@ -0,0 +1,90 @@
+package com.youtube_clone.subscription.services;
+
+import com.youtube_clone.subscription.entities.Subscription;
+import com.youtube_clone.subscription.repositories.SubscriptionRepository;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.UUID;
+
+@Service
+public class SubscriptionServiceImpl implements SubscriptionService {
+    private final SubscriptionRepository repository;
+
+    public SubscriptionServiceImpl(SubscriptionRepository repository) {
+        this.repository = repository;
+    }
+
+
+    /** This method makes sure that user is subscribed to the creator.
+     * We do not have the constructor with 4 values in the Subscription class,
+     * that is the reason we are using the Builder pattern and
+     * assigning the values at run time, and it adds them.
+     * That is the reason we do not need to create manual constructor at the Subscription class.
+     * The orElseGet() -> This method is Optional class method where it checks if there is already
+     * subscribed user if there is already subscribed user, then it returns that user that moment.
+     * If there is not any already subscribed, then it goes ahead and create new
+     * subscriber and goes ahead with creating new subscribe
+     * @param userId    unique id for the user
+     * @param creatorId unique id for the creator
+     * @return return the object of Subscription.
+     */
+    @Override
+    public Subscription subscribe(UUID userId, UUID creatorId) {
+        return repository.findByUserIdAndCreatorId(userId, creatorId)
+                .orElseGet(() -> {
+                    Subscription sub = Subscription.builder()
+                            .userId(userId).creatorId(creatorId)
+                            .subscribedAt(LocalDateTime.now())
+                            .active(true)
+                            .build();
+                    return repository.save(sub);
+                });
+    }
+
+    /** This method makes sure that we are getting the checking from the database
+     * if the user is subscribed to the creator or not.
+     * if a user is subscribed then we change the state to false,
+     * it makes sure that entry is changed in the table
+     * then we save this in table.
+     * @param userId unique id of the user
+     * @param creatorId unique id of the Creator.
+     */
+    @Override
+    public void unsubscribe(UUID userId, UUID creatorId) {
+        repository.findByUserIdAndCreatorId(userId, creatorId)
+                .ifPresent(sub -> {
+                    sub.setActive(false);
+                    repository.save(sub);
+                });
+    }
+
+    /** Gives back the list of Subscribers when provided with the userId
+     * @param userId unique id for user.
+     * @return List of Subscriptions.
+     */
+    @Override
+    public List<Subscription> getSubscribersByUserId(UUID userId) {
+        return repository.findByUserId(userId);
+    }
+
+    /** Gives back the list of Subscribers for the creator.
+     * @param creatorId unique id for the creator.
+     * @return List of Subscriptions.
+     */
+    @Override
+    public List<Subscription> getSubscribersByCreatorId(UUID creatorId) {
+        return repository.findByCreatorId(creatorId);
+    }
+
+    /** It checks if the user has subscribed to the creator or not.
+     * @param userId unique id for of the user.
+     * @param creatorId unique id for the creator.
+     * @return state true or false.
+     */
+    @Override
+    public boolean isUserSubscribed(UUID userId, UUID creatorId) {
+        return repository.findByUserIdAndCreatorId(userId, creatorId).isPresent();
+    }
+}
