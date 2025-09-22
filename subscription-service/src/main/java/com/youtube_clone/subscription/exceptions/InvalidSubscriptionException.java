937f49b9482e18f11a74af01e3dcd5d8854a12f6
kartik-04 <04kartik04@gmail.com>
2025-09-22 19:39:23 +0530

    feat : implemented the exception class if there invalid subscription

---
+++ subscription-service/src/main/java/com/youtube_clone/subscription/exceptions/InvalidSubscriptionException.java
@@ -0,0 +1,7 @@
+package com.youtube_clone.subscription.exceptions;
+
+public class InvalidSubscriptionException extends RuntimeException {
+  public InvalidSubscriptionException(String message) {
+    super(message);
+  }
+}
