709c6ddbb7252593531626f73177bfb6bf96a543
kartik-04 <04kartik04@gmail.com>
2025-09-22 19:40:21 +0530

    feat : this pipeline includes the rules classes

---
+++ subscription-service/src/main/java/com/youtube_clone/subscription/validation/ValidationPipeline.java
@@ -0,0 +1,20 @@
+package com.youtube_clone.subscription.validation;
+
+import com.youtube_clone.subscription.entities.Subscription;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ValidationPipeline {
+    private final List<ValidationRule> validationRules = new ArrayList<>();
+
+    public void addRules(ValidationRule rule){
+        validationRules.add(rule);
+    }
+
+    public void validate(Subscription subscription) {
+        for(ValidationRule rule : validationRules){
+            rule.validate(subscription);
+        }
+    }
+}
