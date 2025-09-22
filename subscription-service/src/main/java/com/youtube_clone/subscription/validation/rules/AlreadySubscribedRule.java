98fc26b366dd6f05077086483ccbfe114adc5370
kartik-04 <04kartik04@gmail.com>
2025-09-22 19:40:42 +0530

    feat : rule for the pipeline

---
+++ subscription-service/src/main/java/com/youtube_clone/subscription/validation/rules/AlreadySubscribedRule.java
@@ -0,0 +1,25 @@
+package com.youtube_clone.subscription.validation.rules;
+
+import com.youtube_clone.subscription.entities.Subscription;
+import com.youtube_clone.subscription.exceptions.AlreadySubscribedException;
+import com.youtube_clone.subscription.repositories.SubscriptionRepository;
+import com.youtube_clone.subscription.validation.ValidationRule;
+
+public class AlreadySubscribedRule implements ValidationRule {
+    /** Checks for the user id if already subscribed to the creatorId
+     * @param subscription object of Subscriber
+     */
+    SubscriptionRepository subscriptionRepository;
+
+    @Override
+    public void validate(Subscription subscription) {
+        subscriptionRepository.findByUserIdAndCreatorId(subscription.getUserId(), subscription.getCreatorId())
+                .ifPresent(existing ->{
+                    if(existing.isActive()) {
+                        throw new AlreadySubscribedException(
+                                "User is already subscribed to this creator."
+                        );
+                    }
+                });
+    }
+}
