a7cb93692d144d1ca17049061aa3c2957fbd4ca8
kartik-04 <04kartik04@gmail.com>
2025-10-02 20:36:42 +0530

    refactor : clean-up

--- subscription-service/src/main/java/com/youtube_clone/subscription/dtos/CreateSubscriptionRequest.java
+++ subscription-service/src/main/java/com/youtube_clone/subscription/dtos/CreateSubscriptionRequest.java
@@ -3,11 +3,14 @@ package com.youtube_clone.subscription.dtos;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import jakarta.validation.constraints.NotNull;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 public class CreateSubscriptionRequest {
+
+    @NotNull(message = "UserId cannot be null")
     private String userId;
     private String creatorId;
 }
