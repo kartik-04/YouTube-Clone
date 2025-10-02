5b1d2399fcdcdbb35033cfe8f5d7841e52a2683a
kartik-04 <04kartik04@gmail.com>
2025-10-02 20:36:55 +0530

    refactor : added the new functionality

--- subscription-service/src/main/java/com/youtube_clone/subscription/exceptions/GlobalExceptionHandler.java
+++ subscription-service/src/main/java/com/youtube_clone/subscription/exceptions/GlobalExceptionHandler.java
@@ -3,11 +3,23 @@ package com.youtube_clone.subscription.exceptions;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
 @ControllerAdvice
 public class GlobalExceptionHandler{
+
+  @ExceptionHandler(MethodArgumentNotValidException.class)
+  public ResponseEntity<String> handleValidationErrors(MethodArgumentNotValidException ex) {
+    String errorMsg = ex.getBindingResult()
+            .getFieldErrors()
+            .stream()
+            .map(err -> err.getField() + " " + err.getDefaultMessage())
+            .findFirst()
+            .orElse("Invalid input");
+    return ResponseEntity.badRequest().body(errorMsg);
+  }
 
   @ExceptionHandler(SubscriptionNotFoundException.class)
   public ResponseEntity<String> handleNotFound(SubscriptionNotFoundException ex){
