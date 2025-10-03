c8cb3159452ca645e65c57dd4e117f7c68a6c0df
kartik-04 <04kartik04@gmail.com>
2025-10-03 23:54:54 +0530

    refactor : changed the @annotation to the Repository

--- subscription-service/src/main/java/com/youtube_clone/subscription/repositories/SubscriptionRepository.java
+++ subscription-service/src/main/java/com/youtube_clone/subscription/repositories/SubscriptionRepository.java
@@ -3,12 +3,13 @@ package com.youtube_clone.subscription.repositories;
 import com.youtube_clone.subscription.entities.Subscription;
 import jdk.jfr.Registered;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
-@Registered
+@Repository
 public interface SubscriptionRepository extends JpaRepository<Subscription, UUID> {
     List<Subscription> findByUserId(UUID userId);
     List<Subscription> findByCreatorId(UUID creatorId);
