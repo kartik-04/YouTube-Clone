0d06ed54509928da477257253b86e2abcfc97ee5
kartik-04 <04kartik04@gmail.com>
2025-10-07 04:30:51 +0530

    chore : These are the setup files for the reacton service

---
+++ reaction/src/test/java/com/youtube_clone/reaction/ReactionApplicationTests.java
@@ -0,0 +1,13 @@
+package java.com.youtube_clone.reaction;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReactionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
