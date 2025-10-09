722a4547dfac7b373e9dfdcc0ba0b7de47525cfe
kartik-04 <04kartik04@gmail.com>
2025-10-09 12:10:38 +0530

    feat : Entry point for the Reaction service

--- Reaction/src/main/java/com/youtube_clone/ReactionApplication.java
+++ Reaction/src/main/java/com/youtube_clone/ReactionApplication.java
@@ -1,4 +1,11 @@
 package com.youtube_clone;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class ReactionApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ReactionApplication.class, args);
+    }
 }
