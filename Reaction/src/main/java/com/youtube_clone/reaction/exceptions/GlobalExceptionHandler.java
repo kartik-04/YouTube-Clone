57797f2cc3c26bf7d138cb3cbcbe3fdc8fad024c
kartik-04 <04kartik04@gmail.com>
2025-10-10 13:49:17 +0530

    chore : clean-up

--- Reaction/src/main/java/com/youtube_clone/reaction/exceptions/GlobalExceptionHandler.java
+++ Reaction/src/main/java/com/youtube_clone/reaction/exceptions/GlobalExceptionHandler.java
@@ -6,11 +6,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.context.request.WebRequest;
 
-import java.time.LocalDateTime;
-import java.util.HashMap;
-import java.util.Map;
 import java.util.stream.Collectors;
 
 @ControllerAdvice
