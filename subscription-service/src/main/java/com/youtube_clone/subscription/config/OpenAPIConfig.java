281de7931aa3d4d78471b93115d305436d639944
kartik-04 <04kartik04@gmail.com>
2025-10-10 23:22:00 +0530

    chore : Added the javadoc to it

--- subscription-service/src/main/java/com/youtube_clone/subscription/config/OpenAPIConfig.java
+++ subscription-service/src/main/java/com/youtube_clone/subscription/config/OpenAPIConfig.java
@@ -1,14 +1,24 @@
 package com.youtube_clone.subscription.config;
 
-import io.swagger.v3.oas.models.info.Info;
-import io.swagger.v3.oas.models.info.Contact;
 import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+/**
+ * OpenAPI/Swagger configuration for the Subscription service.
+ * Defines API metadata (title, version, description, contact) for Swagger UI.
+ */
 @Configuration
 public class OpenAPIConfig {
 
+    /**
+     * Creates and configures the {@link OpenAPI} bean used by springdoc-openapi to
+     * generate the OpenAPI specification and Swagger UI.
+     *
+     * @return an {@link OpenAPI} instance with service title, version, description, and contact info
+     */
     @Bean
     public OpenAPI customOpenAPI() {
         return new OpenAPI()
