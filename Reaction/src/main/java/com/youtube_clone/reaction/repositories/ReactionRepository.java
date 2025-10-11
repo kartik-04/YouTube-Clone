9b52a0b882efa1a75af1c5a0a4a90c189541ba33
kartik-04 <04kartik04@gmail.com>
2025-10-12 01:12:17 +0530

    refactor : added the @Param() annotation in the method signature

--- Reaction/src/main/java/com/youtube_clone/reaction/repositories/ReactionRepository.java
+++ Reaction/src/main/java/com/youtube_clone/reaction/repositories/ReactionRepository.java
@@ -5,6 +5,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -26,18 +27,18 @@ public interface ReactionRepository extends JpaRepository<Reaction, UUID> {
     Page<Reaction> findByCommentIdAndActiveTrue(UUID commentId, Pageable pageable);
 
     @Query("""
-           SELECT r.type, COUNT(r) 
-           FROM Reaction r 
-           WHERE r.videoId = :videoId AND r.active = true 
+           SELECT r.type, COUNT(r)
+           FROM Reaction r
+           WHERE r.videoId = :videoId AND r.active = true
            GROUP BY r.type
            """)
-    List<Object[]> countReactionsByVideoId(UUID videoId);
+    List<Object[]> countReactionsByVideoId(@Param("videoId") UUID videoId);
 
     @Query("""
-           SELECT r.type, COUNT(r) 
-           FROM Reaction r 
-           WHERE r.commentId = :commentId AND r.active = true 
+           SELECT r.type, COUNT(r)
+           FROM Reaction r
+           WHERE r.commentId = :commentId AND r.active = true
            GROUP BY r.type
            """)
-    List<Object[]> countReactionByCommentId(UUID commentID);
+    List<Object[]> countReactionByCommentId(@Param("commentId") UUID commentID);
 }
