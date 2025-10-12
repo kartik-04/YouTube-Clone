23f2770c4b666c03eb09f0873687b09c2a83e534
kartik-04 <04kartik04@gmail.com>
2025-10-12 05:35:15 +0530

    refactor : Added more validation pipeline

--- Reaction/src/main/java/com/youtube_clone/reaction/dtos/CreateReactionRequest.java
+++ Reaction/src/main/java/com/youtube_clone/reaction/dtos/CreateReactionRequest.java
@@ -1,6 +1,8 @@
 package com.youtube_clone.reaction.dtos;
 
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -13,11 +15,18 @@ import lombok.NoArgsConstructor;
 public class CreateReactionRequest {
 
     @NotNull(message = "UserId cannot be null")
+    @NotBlank(message = "UserId cannot be blank")
     private String userId;
 
     private String videoId;
     private String commentId;
 
     @NotNull(message = "ReactionType cannot be null")
+    @NotBlank(message = "ReactionType cannot be blank")
+    @Pattern(
+            regexp = "LIKE|DISLIKE|LOVE|SAD|ANGRY",
+            flags = Pattern.Flag.CASE_INSENSITIVE,
+            message = "Invalid reaction type"
+    )
     private String type; // e.g., LIKE, DISLIKE
 }
