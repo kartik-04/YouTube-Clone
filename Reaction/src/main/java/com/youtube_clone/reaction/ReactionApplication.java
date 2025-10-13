8a5eb1168fa3624c85df78ef11eaf7a7221e6613
kartik-04 <04kartik04@gmail.com>
2025-10-13 14:46:04 +0530

    refactor : added the JpaAuditing

--- Reaction/src/main/java/com/youtube_clone/reaction/ReactionApplication.java
+++ Reaction/src/main/java/com/youtube_clone/reaction/ReactionApplication.java
@@ -2,8 +2,10 @@ package com.youtube_clone.reaction;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class ReactionApplication {
     public static void main(String[] args) {
         SpringApplication.run(ReactionApplication.class, args);
