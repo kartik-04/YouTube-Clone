dcb9655c10046131e7cc169bfd390e078a5a7a63
kartik-04 <04kartik04@gmail.com>
2025-10-13 21:32:42 +0530

    feat : entry point for the service

---
+++ watch-history/src/main/java/com/youtube_clone/watchHistory/WatchHistoryApplication.java
@@ -0,0 +1,13 @@
+package com.youtube_clone.watchHistory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WatchHistoryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WatchHistoryApplication.class, args);
+    }
+
+}
