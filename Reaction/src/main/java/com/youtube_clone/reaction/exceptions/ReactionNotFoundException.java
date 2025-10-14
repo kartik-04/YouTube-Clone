fb0a91a156f0797feb78288a37227a8ccad7fbfe
kartik-04 <04kartik04@gmail.com>
2025-10-15 02:52:45 +0530

    chore : changes to file name

--- Reaction/src/main/java/com/youtube_clone/reaction/exceptions/ReactionNotFoundException.java
+++ Reaction/src/main/java/com/youtube_clone/reaction/exceptions/ReactionNotFoundException.java
@@ -1,4 +1,7 @@
 package com.youtube_clone.reaction.exceptions;
 
-public class ReactionNotFoundException {
+public class ReactionNotFoundException extends RuntimeException{
+    public ReactionNotFoundException(String message){
+        super(message);
+    }
 }
