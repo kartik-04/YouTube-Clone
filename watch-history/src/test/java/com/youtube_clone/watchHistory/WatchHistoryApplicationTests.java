fdbbc85deeb3e684c96fe0137d3c6491b81b9779
kartik-04 <04kartik04@gmail.com>
2025-10-14 23:53:47 +0530

    chore : clean-up

---
+++ watch-history/src/test/java/com/youtube_clone/watchHistory/WatchHistoryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.youtube_clone.watchHistory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WatchHistoryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
