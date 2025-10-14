57c3ffbbb4da21d4f68d87731badeb9a2a699d98
kartik-04 <04kartik04@gmail.com>
2025-10-15 03:20:08 +0530

    chore : clean-up

--- watch-history/src/main/java/com/youtube_clone/watchHistory/exceptions/GlobalExceptionHandler.java
+++ watch-history/src/main/java/com/youtube_clone/watchHistory/exceptions/GlobalExceptionHandler.java
@@ -1,18 +1,11 @@
 package com.youtube_clone.watchHistory.exceptions;
 
-import ch.qos.logback.core.spi.ErrorCodes;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 @ControllerAdvice
 public class GlobalExceptionHandler {
-
-    @ExceptionHandler(MethodArgumentNotValidException.class)
-    public ResponseEntity<ApiResponse<Void>> handleValidationError(MethodArgumentNotValidException ex){
-        return null;
-    }
 
     @ExceptionHandler(InvalidWatchHistoryRequestException.class)
     public ResponseEntity<ApiResponse<Void>> handleInvalidWatchHistoryRequestException(InvalidWatchHistoryRequestException ex){
@@ -30,5 +23,11 @@ public class GlobalExceptionHandler {
     public ResponseEntity<ApiResponse<Void>> handleWatchHistoryPersistenceException(WatchHistoryPersistenceException ex){
         return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
                 .body(ApiResponse.error("Watch history persistence error", ex.getMessage()));
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ApiResponse<Void>> handleException(Exception ex){
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                .body(ApiResponse.error("Internal server error", ex.getMessage()));
     }
 }
