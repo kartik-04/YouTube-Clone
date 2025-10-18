7c55af3b71ca28cc567b51c0b7d87ecff9b11451
kartik-04 <04kartik04@gmail.com>
2025-10-18 20:22:32 +0530

    fix : added few annotation for JpaAuditing and EntityScan and EnableJpaRepositories

--- watch-history/src/main/java/com/youtube_clone/watchHistory/WatchHistoryApplication.java
+++ watch-history/src/main/java/com/youtube_clone/watchHistory/WatchHistoryApplication.java
@@ -2,8 +2,14 @@ package com.youtube_clone.watchHistory;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
+@EnableJpaAuditing
 @SpringBootApplication
+@EntityScan(basePackages = "com.youtube_clone.watchHistory.entities")
+@EnableJpaRepositories(basePackages = "com.youtube_clone.watchHistory.repositories")
 public class WatchHistoryApplication {
 
     public static void main(String[] args) {
