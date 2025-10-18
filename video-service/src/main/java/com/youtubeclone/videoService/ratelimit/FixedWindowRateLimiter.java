ab88ade105b1b242833db5d713696ec3f7eace6e
kartik-04 <04kartik04@gmail.com>
2025-10-19 05:26:21 +0530

    feat : This is Window Rate limiter class which sets the number of request allowed per minute

---
+++ video-service/src/main/java/com/youtubeclone/videoService/ratelimit/FixedWindowRateLimiter.java
@@ -0,0 +1,41 @@
+package com.youtubeclone.videoService.ratelimit;
+
+import java.time.Instant;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
+
+public class FixedWindowRateLimiter implements RateLimiter {
+    private final int limit;
+    private final long windowMillis;
+    private final Map<UUID, UserBucket> buckets = new HashMap<>();
+
+    public FixedWindowRateLimiter(int limit, long windowMillis) {
+        this.limit = limit;
+        this.windowMillis = windowMillis;
+    }
+
+    /**
+     * @param videoId an Unique qualifier for video
+     */
+    @Override
+    public boolean allowRequest(UUID videoId) {
+        long now = Instant.now().toEpochMilli();
+        buckets.putIfAbsent(videoId, new UserBucket());
+        UserBucket bucket = buckets.get(videoId);
+        if(now - bucket.windowStart >= windowMillis){
+            bucket.windowStart = now;
+            bucket.count = 0;
+        }
+        if(bucket.count < limit){
+            bucket.count++;
+            return true;
+        }
+        return false;
+    }
+
+    private static class UserBucket{
+        int count;
+        long windowStart;
+    }
+}
