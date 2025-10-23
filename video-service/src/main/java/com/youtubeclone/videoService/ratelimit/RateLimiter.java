3413d51a6bee5cef889fe5f2c59866b0391283c6
kartik-04 <04kartik04@gmail.com>
2025-10-23 05:50:50 +0530

    refactor : change the name of videoId to key cause we are also using the creatorId with this method allowRequest

--- video-service/src/main/java/com/youtubeclone/videoService/ratelimit/RateLimiter.java
+++ video-service/src/main/java/com/youtubeclone/videoService/ratelimit/RateLimiter.java
@@ -3,5 +3,5 @@ package com.youtubeclone.videoService.ratelimit;
 import java.util.UUID;
 
 public interface RateLimiter {
-    boolean allowRequest(UUID videoId);
+    boolean allowRequest(UUID key);             // key may represent creatorId or videoId depending on context.
 }
