a35f47fe089fb1ed86941117407a57b4db93a508
kartik-04 <04kartik04@gmail.com>
2025-10-23 05:54:29 +0530

    chore : clean-up

--- video-service/src/test/java/com/youtubeclone/videoService/VideoServiceApplicationTests.java
+++ video-service/src/test/java/com/youtubeclone/videoService/VideoServiceApplicationTests.java
@@ -1,9 +1,13 @@
 package com.youtubeclone.videoService;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.AutoConfigureDataJpa;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@DataJpaTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 class VideoServiceApplicationTests {
 
     @Test
