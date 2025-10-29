786314051a7904eb2d755cd04ec08ab5b0bf26e3
kartik-04 <04kartik04@gmail.com>
2025-10-29 22:47:42 +0530

    chore : clean-up

--- video-service/src/test/java/com/youtubeclone/videoService/VideoControllerIntegrationTest.java
+++ video-service/src/test/java/com/youtubeclone/videoService/VideoControllerIntegrationTest.java
@@ -1,6 +1,11 @@
 package com.youtubeclone.videoService;
 
+import com.youtubeclone.videoService.repositories.MetadataRepository;
+import com.youtubeclone.videoService.repositories.VideoRepository;
+import com.youtubeclone.videoService.services.VideoFileService;
+import com.youtubeclone.videoService.services.VideoMetadataService;
 import org.junit.jupiter.api.*;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
@@ -10,5 +15,15 @@ import org.springframework.test.context.ActiveProfiles;
 @ActiveProfiles("test") // activates application-test.properties
 @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 class VideoControllerIntegrationTest {
-    
+    @Autowired
+    private VideoFileService videoFileService;
+
+    @Autowired
+    private VideoMetadataService videoMetadataService;
+
+    @Autowired
+    private VideoRepository videoRepository;
+
+    @Autowired
+    private MetadataRepository metadataRepository;
 }
